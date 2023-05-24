a63af062a7e524170e73476778d2caeedb1acde4
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-05-24 10:39:54 +0200

    Add AuthenticationProviderUserPassword

---
+++ src/main/java/com/example/security/AuthenticationProviderUserPassword.java
@@ -0,0 +1,42 @@
+package com.example.security;
+
+import com.example.repository.UserRepository;
+import com.example.service.UserService;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.security.authentication.AuthenticationProvider;
+import io.micronaut.security.authentication.AuthenticationRequest;
+import io.micronaut.security.authentication.AuthenticationResponse;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+import java.util.Map;
+
+@Singleton
+public class AuthenticationProviderUserPassword implements AuthenticationProvider {
+
+    @Inject
+    UserRepository repository;
+
+    @Inject
+    UserService userService;
+
+    @Override
+    public Publisher<AuthenticationResponse> authenticate(HttpRequest<?> httpRequest, AuthenticationRequest<?, ?> authenticationRequest) {
+        return Flux.create(emitter -> {
+            String username = authenticationRequest.getIdentity().toString();
+            String password = authenticationRequest.getSecret().toString();
+            boolean isValid = userService.validCredentials(username,password);
+            if (isValid) {
+                emitter.next(AuthenticationResponse.success(username, Map.of(
+                        "sub", repository.findByUsername(username).getUserId(),
+                        "iss", "http://localhost:8000/auth")));
+                emitter.complete();
+            } else {
+                emitter.error(AuthenticationResponse.exception());
+            }
+        }, FluxSink.OverflowStrategy.ERROR);
+    }
+}
